1118e5c70393069a854ac8c83ad0e82e2b448197
Dani Coll <daniel.coll@dynatrace.com>
2021-12-07 11:24:24 +0100

    7th problem

---
+++ 7/7-2.js
@@ -0,0 +1,50 @@
+const readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+rl.on('line', (input) => {
+    processLine(input);
+});
+
+rl.on('close', () => {});
+
+function processLine(input) {
+    const positions = input.split(',').map((s) => Number(s));
+
+    positions.sort((a, b) => a - b);
+
+    let mid = positions[positions.length - 1] / 2;
+
+    let fuelCost = calculateFuelCost(positions, mid, 0, positions[positions.length - 1]);
+
+    console.log(fuelCost);
+}
+
+function calculateFuelCost(positions, position, min, max) {
+    console.log('calculating fuel cost of', position);
+    let currentFuelCost = getFuelCostToPosition(positions, position);
+    let nextFuelCost = getFuelCostToPosition(positions, position + 1);
+    let previousFuelCost = getFuelCostToPosition(positions, position - 1);
+    if (nextFuelCost < currentFuelCost) {
+        const newPosition = Math.floor((max + position) / 2);
+        return calculateFuelCost(positions, newPosition, position, max);
+    }
+    if (previousFuelCost < currentFuelCost) {
+        return calculateFuelCost(positions, Math.floor((position + min) / 2), min, position);
+    }
+    return currentFuelCost;
+}
+
+function getFuelCostToPosition(positions, position) {
+    let fuelCost = 0;
+    for (let i = 0; i < positions.length; ++i) {
+        for (let j = 0; j < Math.abs(position - positions[i]); ++j) {
+            fuelCost += j + 1;
+        }
+    }
+    return fuelCost;
+}
